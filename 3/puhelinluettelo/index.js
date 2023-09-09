273d8c70555a3f6d8f569d2e0c324a7da7c737b4
Kalle Nyman <kalle.nyman@tuni.fi>
2023-09-09 22:35:23 +0300

    3.3

--- 3/puhelinluettelo/index.js
+++ 3/puhelinluettelo/index.js
@@ -26,6 +26,16 @@ app.get("/api/persons", (req, res) => {
     res.json(persons)
 })
 
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if( person ) {
+        res.json(person)
+    } else {
+        res.status(404).end("Not Found")
+    }
+})
+
 app.get("/info", (req, res) => {
     res.end(`Phonebook has info for ${persons.length} people.\n${Date(Date.now())}`)
 })
