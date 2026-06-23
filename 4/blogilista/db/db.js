e25c371330e5123976f73f308e10ed2cc7c09ce4
Kalle Nyman <kalle.nyman@tuni.fi>
2026-06-23 21:10:58 +0300

    4.1

--- 4/blogilista/db/db.js
+++ 4/blogilista/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connectToMongo = () => {
-    const url = process.env.MONGO_URI
+	const url = process.env.MONGO_URI
 	mongoose.connect(url)
 		.then(() => {
 			console.log("Connected to MongoDB")
